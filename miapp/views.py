5d4f88af1f09d4baa5c861eaf85fa824376b0966
S8Vega <bsebastianvg18@gmail.com>
2020-08-30 22:20:05 -0500

    navegacion entre rutas

--- miapp/views.py
+++ miapp/views.py
@@ -1,20 +1,37 @@
 from django.shortcuts import render, HttpResponse
 
-# Create your views here.
-
+layout = """
+    <h1>sitio web con Django</h1>
+    <hr/>
+    <ul>
+        <li>
+            <a href="/inicio">Inicio</a>
+        </li>
+        <li>
+            <a href="/hola-mundo">hola mundo</a>
+        </li>
+        <li>
+            <a href="/pagina-pruebas">pagina de pruebas</a>
+        </li>
+    </ul>
+    <hr/>
+"""
 def index(request):
-    return HttpResponse("""
+    html = """
         <h1>Inicio</h1>
-    """)
+    """
+    return HttpResponse(layout + html)
 
 def hola_mundo(request):
-    return HttpResponse("""
+    html = """
         <h1>hola mundo</h1>
         <h3>soy Sebastian</h3>
-    """)
+    """
+    return HttpResponse(layout + html)
 
 def pagina(request):
-    return HttpResponse("""
+    html = """
         <h1>Pagina de mi web</h1>
         <p>creado por Sebastian Vega</p>
-    """)
+    """
+    return HttpResponse(layout + html)
